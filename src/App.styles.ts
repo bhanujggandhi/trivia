1363b3b4f1664234373e2d94cb2aa811839f9a58
bhanujggandhi <gandhibhanuj@gmail.com>
2020-10-31 16:39:28 +0530

    init

---
+++ src/App.styles.ts
@@ -0,0 +1,98 @@
+import styled, { createGlobalStyle } from 'styled-components'
+import BgImage from './images/bg.jpg'
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+    background-image: url(${BgImage});
+    background-size: cover;
+    margin: 0;
+    padding: 0 20px;
+    display: flex;
+    justify-content: center;
+  }
+
+  * {
+    box-sizing: border-box;
+    font-family: 'Catamaran', sans-serif;
+  }
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    color: #fff;
+  }
+
+  .score {
+    color: #fff;
+    font-size: 2rem;
+    margin: 0;
+  }
+
+  h1 {
+    font-family: 'Fascinate Inline', Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    background-size: 100%;
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    filter: drop-shadow(2px 2px #0085a3);
+    font-weight: 400;
+    font-size: 70px;
+    text-align: center;
+    margin: 20px
+  }
+
+  .start, .next {
+    cursor: pointer;
+    background: linear-gradient(180deg, #fff, #ffcc91);
+    border: 2px solid #d38558;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 40px;  
+    }
+
+  .start {
+      max-width: 200px;
+    }
+`
+
+export const SpinnerOverlay = styled.div`
+  /* height: 60vh; */
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SpinnerContainer = styled.div`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border: 3px solid rgba(256, 256, 356, 1);
+  border-radius: 50%;
+  border-top-color: #636767;
+  animation: spin 1s ease-in-out infinite;
+  -webkit-animation: spin 1s ease-in-out infinite;
+  @keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+`;
